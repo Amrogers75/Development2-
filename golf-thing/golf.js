cde85ce9e38bafa07cc0e4d544d9e0120ade19f5
Anthony Rogers <amrogers75@gmail.com>
2016-06-02 09:19:05 -0600

    still having a VM692:1 Uncaught SyntaxError: Unexpected token A in JSON at position 0

--- golf-thing/golf.js
+++ golf-thing/golf.js
@@ -3,6 +3,41 @@ var numholes = 18;
 var teetime = 45;
 var seconds = 59;
 
+var testCourse ={};
+var colseCourses ={};
+var xhttp = new XMLHttpRequest();
+var local_obj = {latiude: 40.4426135, longitude: -111.8631116, radius: 30};
+
+// API call for courses
+
+function loadMe(){
+    $.post("http://golf-courses-api.herokuapp.com/courses",local_obj,function(data,status){
+        colseCourses = JSON.parse(data);
+        console.log(data);
+        for (var p in closeCourese.courses){
+            var thisCourse = document.createElement("div");
+            thisCourse.setAttribute('id');
+            var mydisplay = "<div id='" + closeCourese.courses[p].id +"' class='thisCourse' onclick='getCourseInfo(this.id)'>"+ closeCourese.courses[p].name+"</div>";
+            $("#slectCourse").append(mydisplydiv);
+        }
+        document.getElementById('golfDiv').style.display = 'block';
+    });
+}
+
+function getCourseInfo(id){
+    xhttp = new XMLHttpRequest;
+    xhttp.onreadystatechange = function(){
+        if (xhttp.readyState == 4 && xhttp.status == 200){
+            testCourse = JSON.parse(xhttp.responseText);
+            console.log(testCourse.course.name);
+            $("#golfcourselabel").html(testCourse.name);
+        }
+    };
+    xhttp.open("http://golf-courses-api.herokuapp.com/courses/" + id,true);
+    xhttp.send();
+}
+
+// score card build
 function buildcard(){
     beginTimer();
     var holecollection = "";
@@ -58,34 +93,3 @@ function stopTimer() {
     document.getElementById("stop").innerHTML = d.toLocaleTimeString();
 } */
 
-var testCourse ={};
-var colseCourses ={};
-var xhttp = new XMLHttpRequest();
-
-var local_obj = {latiude: 40.4426135, longitude: -111.8631116, radius: 100};
-
-function loadMe(){
-    $.post("http://golf-courses-api.herokuapp.com/courses",local_obj,function(data,status){
-        colseCourses = JSON.parse(data);
-        for (var p in closeCourese.courses){
-            var thisCourse = document.createElement("div");
-            thisCourse.setAttribute('id');
-            var mydisplaydiv = "<div id='" + closeCourese.courses[p].id +"' class='thisCourse' onclick='getCourseInfo(this.id)'>"+ closeCourese.courses[p].name+"</div>"
-            $("#slectCourse").append(mydisplydiv);
-        }
-        document.getElementById('golfDiv').style.display = 'block';
-    });
-};
-
-function getCourseInfo(id){
-    xhttp = new XMLHttpRequest;
-    xhttp.onreadystatechange = function(){
-        if (xhttp.readyState == 4 && xhttp.status == 200){
-            testCourse = JSON.parse(xhttp.responseText);
-            console.log(testCourse.course.name);
-            $("#golfcourselabel").html(testCourse.name);
-        }
-    };
-    xhttp.open("http://golf-courses-api.herokuapp.com/courses/" + id,true);
-    xhttp.send();
-}
