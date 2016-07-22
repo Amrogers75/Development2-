a4a83e41c6e95d6f611c30fad535cf97512283fb
Anthony Rogers <amrogers75@gmail.com>
2016-07-22 09:28:41 -0600

    added task1 and got it to show up

--- AngularJS/services/tasks.service.js
+++ AngularJS/services/tasks.service.js
@@ -1,24 +1,109 @@
 /**
  * Created by anthonyrogers on 7/20/16.
  */
-function () {
+(function () {
 
     angular.module('myApp')
-    //.service('taskService', taskService);
-        .service('todoCtrl', todoCtrl);
-    
-    /*function taskService() {
-     var self = this;
-     self.selectedtask = undefined;
-     //self.getRandomTask = getRandomTask;
-     }*/
+        .service('todoService', todoService);
 
-    function todoCtrl() {
+    function todoService() {
         var self = this;
-        self.todoList = [{todoText: 'Hi Mom', done: false}];
+        //self.todoInput = '';
+        self.todoList = [{todoText: 'Lawn', done: false}];
 
-        self.todoAdd = function () {
-            self.todoList.push({todoText: self.todoInput, done: false});
+        self.todoAdd = function (todoInput) {
+            self.todoList.push({todoText: todoInput, done: false});
             self.todoInput = "";
-        }
+        };
+
+        self.remove = function () {
+            var oldList = self.todoList;
+            self.todoList = [];
+            angular.forEach(oldList, function (x) {
+                if (!x.done) self.todoList.push(x);
+            });
+         };
     }
+})();
+
+
+/*
+(function () {
+
+    angular.module('myApp')
+        .service('characterService' name, $http characterService function);
+
+    function characterService() {
+        var self = this;
+        self.selectedChar = undefined;
+        self.getRandomCharacter = getRandomCharacter;
+
+        function getRandomCharacter() {
+            return self.characters[Math.floor(Math.random()*self.characters.length)];
+        }
+
+        function init() {
+            for (var c=0, clen=self.characters.length; c<clen; c++) {
+                self.characters[c].weight = self.characters[c].mass * 2.20462;
+            }
+            self.selectedChar = getRandomCharacter();
+        }
+
+        // sample data from swapi.co
+        self.characters = [
+            {
+                "name": "Luke Skywalker",
+                "height": 172,
+                "mass": 77,
+                "hair_color": "blond",
+                "skin_color": "fair",
+                "eye_color": "blue",
+                "birth_year": "19BBY",
+                "gender": "male"
+            },
+            {
+                "name": "C-3PO",
+                "height": 167,
+                "mass": 75,
+                "hair_color": "n/a",
+                "skin_color": "gold",
+                "eye_color": "yellow",
+                "birth_year": "112BBY",
+                "gender": "n/a"
+            },
+            {
+                "name": "R2-D2",
+                "height": 96,
+                "mass": 32,
+                "hair_color": "n/a",
+                "skin_color": "white, blue",
+                "eye_color": "red",
+                "birth_year": "33BBY",
+                "gender": "n/a"
+            },
+            {
+                "name": "Darth Vader",
+                "height": 202,
+                "mass": 136,
+                "hair_color": "none",
+                "skin_color": "white",
+                "eye_color": "yellow",
+                "birth_year": "41.9BBY",
+                "gender": "male"
+            },
+            {
+                "name": "Leia Organa",
+                "height": 150,
+                "mass": 49,
+                "hair_color": "brown",
+                "skin_color": "light",
+                "eye_color": "brown",
+                "birth_year": "19BBY",
+                "gender": "female"
+            }
+        ];
+
+        init();
+    }
+
+})();*/
--- AngularJS/task1/task1.js
+++ AngularJS/task1/task1.js
@@ -1,36 +1,62 @@
 /**
  * Created by anthonyrogers on 7/20/16.
  */
-(function(){
-var app = angular.module('myApp');
-app.component('task1List', {
-    templateUrl: 'task1/task1.html',
-    controller: todoCtrl
-})
-    .config(task1ListConfig);
 
-function task1ListConfig($stateProvider) {
-    $stateProvider.state('task1', {
-        url: '/task1',
-        template: '<task1-list></task1-list>'
-    });
-}
+//DETAIL
+/*(function(){
 
-function todoCtrl() {
-    var self = this;
-    self.todoList = [{todoText: 'Hi Dad', done: false}];
+ angular.module('myApp')
+ .component('task1', {
+ templateUrl: "task1/task1.html",
+ controller: todoCtrl,
+ bindings: {
+ task1: '<',
+ onClose: '&'
+ }
+ });
 
-    self.todoAdd = function () {
-        self.todoList.push({todoText: self.todoInput, done: false});
-        self.todoInput = "";
-    };
+ function todoCtrl() {
+ var self = this;
+ self.close = close;
 
-    self.remove = function () {
-        var oldList = self.todoList;
-        self.todoList = [];
-        angular.forEach(oldList, function (x) {
-            if (!x.done) self.todoList.push(x);
+ function close() {
+ self.onClose();
+ }
+ }
+
+ })();*/
+
+
+(function () {
+    var app = angular.module('myApp');
+    app.component('task1List', {
+        templateUrl: 'task1/task1.html',
+        controller: todoCtrl
+    })
+        .config(task1ListConfig);
+
+    function task1ListConfig($stateProvider) {
+        $stateProvider.state('task1', {
+            url: '/task1',
+            template: '<task1-list></task1-list>'
         });
-    };
-}
+    }
+
+    function todoCtrl() {
+        var self = this;
+        self.todoList = [{todoText: 'Apples', done: false}];
+
+        self.todoAdd = function () {
+            self.todoList.push({todoText: self.todoInput, done: false});
+            self.todoInput = "";
+        };
+
+        self.remove = function () {
+            var oldList = self.todoList;
+            self.todoList = [];
+            angular.forEach(oldList, function (x) {
+                if (!x.done) self.todoList.push(x);
+            });
+        };
+    }
 })();
